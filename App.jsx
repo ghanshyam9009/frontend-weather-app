dbff1814c569b5185e2b7f4430f38b9cd3c41f5c
Ghanshyam Choudhary <147299599+ghanshyam9009@users.noreply.github.com>
2026-04-01 20:24:19 +0530

    update url

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_BASE_URL = "http://localhost:5000/api/weather";
+const API_BASE_URL = "https://backend-weather-app-anda.onrender.com";
 
 const themeMap = {
   clear: {
